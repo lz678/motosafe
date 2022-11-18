f7a3ca395cbbaebaa9898d5438ba05b0eaefe1d0
李泽 <lize163678@163.com>
2022-11-18 22:16:59 +0800

    chore:init

---
+++ src/AppContent.jsx
@@ -0,0 +1,9 @@
+import Home from './component/Home/Home'
+const AppContent = ()=>{
+    return (
+        <div>
+            <Home/>
+        </div>
+    )
+}
+export default AppContent
---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,9 @@
+import Style from './Home.module.less'
+const Home = ()=>{
+    return (
+        <div className={Style.box}>
+            Home
+        </div>
+    )
+}
+export default Home
