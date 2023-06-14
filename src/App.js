6ea10937236659c634f98ef5f3ce01b8c2b37e13
lz <985256910@qq.com>
2023-06-14 11:41:01 +0800

    chore:添加default

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Provider } from 'react-redux'
 import state from './redux/reducer/MTRoor'
 import AppContent from './AppContent';
+import Apply from "./component/Apply/Apply";
 
 function App() {
   return (
     <Provider store={state}>
-        <AppContent/>
+        {/*<AppContent/>*/}
+        {/*<Apply/>*/}
     </Provider>
   );
 }
